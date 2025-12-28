b7518314e8d46e51bacf4769d54f91af2ee1932f
David Brick <david@davidbrick.com>
2025-12-28 13:58:51 -0500

    Layout options

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var messageString = "When the genius bar needs help, they call you"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .border(.orange, width: 2)
+                .padding()
+            HStack {
+                Button("Awesome") {
+                    messageString = "You are Awesome!"
+                }
+                Button("Great") {
+                    messageString = "You are Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .cornerRadius(10)
+            .border(.purple, width: 2)
+            //               .font(.title2)
+            //             .background(.blue)
         }
-        .padding()
+ //       .padding()
     }
 }
 
