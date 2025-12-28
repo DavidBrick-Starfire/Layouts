4799fa83d6a8535f40f837b0750646d3a72adeac
David Brick <david@davidbrick.com>
2025-12-28 14:35:53 -0500

    colors and spacing

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -8,34 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var messageString = "When the genius bar needs help, they call you"
+    @State var messageString = "When the genius bar needs help, they call you!"
     var body: some View {
-        VStack {
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .border(.orange, width: 2)
+    //    ZStack {
+      //      Color.skyBlue
+//                .ignoresSafeArea()
+            VStack {
+/*                Text("You Have Skills!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(.goldBC)
+                    .padding()
+                    .background(Color("Maroon-BC"))
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+  */              Spacer()
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                //              .border(.orange, width: 2)
+                    .padding()
+                Spacer()
+                HStack {
+                    Button("Awesome") {
+                        messageString = "You are Awesome!"
+                    }
+                    Spacer()
+                    Button("Great") {
+                        messageString = "You are Great!"
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                //          .border(.purple, width: 2)
                 .padding()
-            HStack {
-                Button("Awesome") {
-                    messageString = "You are Awesome!"
-                }
-                Button("Great") {
-                    messageString = "You are Great!"
-                }
+                //               .font(.title2)
+                //             .background(.blue)
             }
-            .buttonStyle(.borderedProminent)
-            .cornerRadius(10)
-            .border(.purple, width: 2)
-            //               .font(.title2)
-            //             .background(.blue)
-        }
- //       .padding()
+//            .background(
+  //              Gradient(colors: [.orange,.yellow]
+    //                     )
+      //          )
+  //      }
+        //       .padding()
     }
 }
 
